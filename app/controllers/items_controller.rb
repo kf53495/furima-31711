ff3ae6ca73dbb39095b63795dd7ec204905fedc1
kf53495 <fkmndk902902@gmail.com>
2020-11-18 19:15:34 +0900

    itemsコントローラーを新規itemが作れるように編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @otem = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_explanation, :category_id, :status_id, :delivery_fee_payer_id, :prefecture_id, :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
+
+
+
 end
