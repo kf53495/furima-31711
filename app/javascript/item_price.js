f2c128c08b23fca6ab91635f4edb8154785cc71c
kf53495 <fkmndk902902@gmail.com>
2020-11-19 11:24:03 +0900

    販売価格を計算して自動で出力するように設定

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,11 @@
-// item-price
-// add-tax-price
-// profit
-
 window.addEventListener('load', function(){
+  const priceInput = document.getElementById("item-price")
 
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value
+    const addTaxPrice = document.getElementById("add-tax-price")
+      addTaxPrice.innerHTML = Math.floor(inputValue * 0.1)
+    const profit = document.getElementById("profit")
+      profit.innerHTML = Math.floor(inputValue - addTaxPrice.innerHTML)
+  })
 })
