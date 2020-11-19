0a7b5959c13b57320bb2e140bfc7eed6be538d34
kf53495 <fkmndk902902@gmail.com>
2020-11-19 15:15:16 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,20 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
+  with_options presence: true do
+    validates :item_name
+    validates :item_explanation
+
+    validates :price, inclusion: { in: 300..9999999 }
+    
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_fee_payer_id
+      validates :delivery_time_id
+    end
+  end
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
