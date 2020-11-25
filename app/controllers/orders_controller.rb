ecae690e670c2046afaf60818e948ae46421dbfa
kf53495 <fkmndk902902@gmail.com>
2020-11-25 22:49:05 +0900

    ルーティング変更に伴いリンク先のURLを変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,17 @@
 class OrdersController < ApplicationController
   def index
-    @item = Item.find_by(id: params[:format])
-   # @order = Order.new
+    #@item = Item.find_by(id: params[:format])
+    @item = Item.find(params[:item_id])
+    #@order = Order.new
   end
 
   def create
-    item = Item.find_by(id: params[:format])
-    @order = Order.new(order_params)
-    destination = Destination.new(destination_params)
+    #@item = Item.find_by(id: params[:format])
+    #@item = Item.find(params[:id])
+    @order = Order.create(order_params)
+    binding.pry
+    destination = Destination.new(destination_params(@order))
+
     if @order.save
       redirect_to root_path
     else
@@ -19,10 +23,10 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:user_id, :item_id).merge(user_id: current_user.id, item_id: item.id)
+    params.permit(:user_id, :item_id) #.merge(user_id: current_user.id, item_id: @item.id)
   end
 
-  def destination_params
+  def destination_params(order)
     params.require(:destination).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id).merge(order_id: order.id)
   end
 
