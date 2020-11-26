d281f195432db1f2233b014f0a4c7b57954d49a5
kf53495 <fkmndk902902@gmail.com>
2020-11-26 11:25:39 +0900

    カラムに値を保存できるようにセッターとゲッターを定義

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,3 +1,4 @@
 class OrderDestination
   include ActiveModel::Model
+  attr_accessor ::postal_code, :user_id, :item_id, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id
 end
