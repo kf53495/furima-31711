a348e2e82138a63723a4dc4db015c00b4471b354
kf53495 <fkmndk902902@gmail.com>
2020-11-26 23:26:54 +0900

    order_destinationモデルを用いて送られてきた内容をそれぞれのカラムに保存できるように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,7 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_destination = OrderDestination.new(order_destination_params)
-    #@destination = Destination.new(destination_params(@order))
     if @order_destination.valid? #@order.valid? && @destination.valid?
-      # @destination.save
       @order_destination.save
       redirect_to root_path
     else
@@ -20,11 +18,7 @@ class OrdersController < ApplicationController
   private
 
   def order_destination_params
-    params.require(:order_destination).permit(:user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id)
+    params.permit(:user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id).merge(user_id: current_user.id, item_id: @item.id)
   end
-
-  # def destination_params(order)
-  #   params.permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id).merge(order_id: @order.id)
-  # end
 
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,18 +1,9 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor ::postal_code, :user_id, :item_id, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id
+  attr_accessor :postal_code, :user_id, :item_id, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id
 
   def save
-    @item = Item.find(params[:item_id])
-    order = Order.create(user_id: current_user.id, item_id: @item.id)
-    Destination.new(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
-
-    # def order_params
-    #   params.permit(:user_id, :item_id).merge(user_id: current_user.id, item_id: @item.id)
-    # end
-  
-    # def destination_params(order)
-    #   params.permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id).merge(order_id: @order.id)
-    # end
+    @order = Order.create(user_id: user_id, item_id: item_id)
+    @destination = Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: @order.id)
   end
 end
