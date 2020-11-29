866add23170933b5adddc0aae4e8f37d72dbd44c
kf53495 <fkmndk902902@gmail.com>
2020-11-29 18:46:01 +0900

    購入済みの商品の編集ページに出品者がアクセスするとトップページに遷移するように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     redirect_to action: :index unless current_user.id == @item.user_id
+    redirect_to action: :index if @item.order
   end
 
   def update
