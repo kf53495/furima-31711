d71733ff2078de85238040fc61c5d14e1452c6c5
kf53495 <fkmndk902902@gmail.com>
2020-11-30 19:21:35 +0900

    order_destinationモデルのテストコード記入

--- spec/factories/order_destinations.rb
+++ spec/factories/order_destinations.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :order_destination do
     token {"tok_abcdefghijk00000000000000000"}
     postal_code {"111-1111"}
-    prefecture_id {1}
-    municipality {"あ市あ町"}
-    address {"あ市1-1-1"}
+    prefecture_id {2}
+    municipality {"あ区あ市"}
+    address {"あ町1-1-1"}
     building_name {"あいうえお"}
     phone_number {"07012345678"}
   end
--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -6,27 +6,48 @@ RSpec.describe OrderDestination, type: :model do
       @order_destination = FactoryBot.build(:order_destination)
     end
 
-    it "配送先の情報として、郵便番号・都道府県・市区町村・番地・電話番号が必須であること" do 
+    it "配送先の情報が全て正しく入力されていたら購入できること" do 
+      expect(@order_destination).to be_valid
     end
-    it "郵便番号が空だと購入できないこと" do 
+    it "郵便番号が空だと購入できないこと" do
+      @order_destination.postal_code = nil
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Postal code can't be blank") 
     end
     it "郵便番号が「数字4文字 ハイフン 数字3文字」の形でないと購入できないこと" do
-      
+      @order_destination.postal_code = "1231234"
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Postal code is invalid") 
     end
     it "都道府県が選択されていないと購入できないこと" do
-      
+      @order_destination.prefecture_id = 1
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Prefecture must be other than 1") 
     end
     it "市区町村が空だとと登録できないこと" do
-      
+      @order_destination.municipality = nil
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Municipality can't be blank") 
     end
     it "番地が空だと購入できないこと" do
-      
+      @order_destination.address = nil
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Address can't be blank") 
     end
-    it "電話番号にはハイフンは不要で、10桁もしくは11桁であること" do
-      
+    it "電話番号にはハイフンは不要であること" do
+      @order_destination.phone_number = "090-1234-1234"
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Phone number is invalid") 
+    end
+    it "電話番号は10桁もしくは11桁であること" do
+      @order_destination.phone_number = "12345"
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Phone number is invalid") 
     end
     it "電話番号が空だと購入できないこと" do
-      
+      @order_destination.phone_number = nil
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Phone number can't be blank") 
     end
   end
 end
