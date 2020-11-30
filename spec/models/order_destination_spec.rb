b13c4d208f9a7bedbf5ad5938473fff420e3251a
kf53495 <fkmndk902902@gmail.com>
2020-11-30 16:21:32 +0900

    order_destinationモデルのテストの準備

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe OrderDestination, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @order_destination = FactoryBot.build(:order_destination)
+    end
+
+    it "配送先の情報として、郵便番号・都道府県・市区町村・番地・電話番号が必須であること" do 
+    end
+    it "郵便番号が空だと購入できないこと" do 
+    end
+    it "郵便番号が「数字4文字 ハイフン 数字3文字」の形でないと購入できないこと" do
+      
+    end
+    it "都道府県が選択されていないと購入できないこと" do
+      
+    end
+    it "市区町村が空だとと登録できないこと" do
+      
+    end
+    it "番地が空だと購入できないこと" do
+      
+    end
+    it "電話番号にはハイフンは不要で、10桁もしくは11桁であること" do
+      
+    end
+    it "電話番号が空だと購入できないこと" do
+      
+    end
+  end
 end
