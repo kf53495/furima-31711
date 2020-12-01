5d9fe52c386930e91d22e1be2934acd85bbeeff9
kf53495 <fkmndk902902@gmail.com>
2020-12-01 10:00:23 +0900

    rubocopの実行

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -4,11 +4,11 @@ class OrderDestination
 
   with_options presence: true do
     validates :token
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
-    validates :municipality 
-    validates :address 
-    validates :phone_number, format: { with:/\A\d{10,11}\z/ }
+    validates :municipality
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
 
   def save
--- spec/factories/order_destinations.rb
+++ spec/factories/order_destinations.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :order_destination do
-    token {"tok_abcdefghijk00000000000000000"}
-    postal_code {"111-1111"}
-    prefecture_id {2}
-    municipality {"あ区あ市"}
-    address {"あ町1-1-1"}
-    building_name {"あいうえお"}
-    phone_number {"07012345678"}
+    token { 'tok_abcdefghijk00000000000000000' }
+    postal_code { '111-1111' }
+    prefecture_id { 2 }
+    municipality { 'あ区あ市' }
+    address { 'あ町1-1-1' }
+    building_name { 'あいうえお' }
+    phone_number { '07012345678' }
   end
 end
