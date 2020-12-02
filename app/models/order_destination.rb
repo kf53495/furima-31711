8058c6b5d0381124b21364a170ecff38a874940b
kf53495 <fkmndk902902@gmail.com>
2020-12-02 19:17:47 +0900

    コントローラー、モデルから重複した表記の削除

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,6 +1,6 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor :postal_code, :user_id, :item_id, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :token
+  attr_accessor :postal_code, :user_id, :item_id, :prefecture_id, :municipality, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :token
