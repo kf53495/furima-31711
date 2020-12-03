a188bd26c7fdbd7935c746246d12bab8fa7329df
kf53495 <fkmndk902902@gmail.com>
2020-12-03 15:08:24 +0900

    バリデーション追加に伴い、テストコード用のFactoryBotを修正

--- spec/factories/order_destinations.rb
+++ spec/factories/order_destinations.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :order_destination do
     token { 'tok_abcdefghijk00000000000000000' }
+    user_id { 1 }
+    item_id { 1 }
     postal_code { '111-1111' }
     prefecture_id { 2 }
     municipality { 'あ区あ市' }
